10c6cf0fc051148a2c8ef33f439fa05aaaff19db
俊佚 谢 <xiejunyi@outlook.com>
2018-12-19 19:29:48 +0800

    chapter 19 (1-5)

---
+++ chapter19/examples.cpp
@@ -0,0 +1,142 @@
+#include "examples.h"
+
+void example1()
+{
+	//a simple set or multiset of integers (using default sort predicate)
+	set <int> set_integers1;
+	multiset <int> mset_integers1;
+
+	//set and multiset instantiated given a user defined sort predicate
+	set <int, SortDesending<int>> set_integers2;
+	multiset <int, SortDesending<int>> mset_integers2;
+
+	//creating one set from another, or part of another container
+	set <int> set_integers3(set_integers1);
+	multiset <int> mset_integers3(mset_integers1.cbegin(), mset_integers1.cend());
+}
+
+void example2()
+{
+	set <int> set_integers;
+	multiset <int> mset_integers;
+
+	set_integers.insert(60);
+	set_integers.insert(-1);
+	set_integers.insert(3000);
+	cout << "Writing the contens of the set to the screen" << endl;
+	DisplayContents(set_integers);
+
+	mset_integers.insert(set_integers.begin(), set_integers.end());
+	mset_integers.insert(3000);
+
+	cout << "Writing the content of the multiset to the screen" << endl;
+	DisplayContents(mset_integers);
+
+	cout << "Number of instances of '3000' in the multiset are:" << " '";
+	cout << mset_integers.count(3000) << "'" << endl;
+}
+
+void example3()
+{
+	set <int> set_integers;
+	set_integers = { 43,78,-1,124 };
+
+	//display set_integers to the screen
+	DisplayContents(set_integers);
+
+	//Try finding an element
+	auto element_found_i = set_integers.find(-1);
+
+	//check if found...
+	if (element_found_i != set_integers.end())
+		cout << "Element " << *element_found_i << " found!" << endl;
+	else
+		cout << "Element not found in set!" << endl;
+
+	//Try finding another element
+	auto another_find_i = set_integers.find(12345);
+
+	//Check if found...
+	if (another_find_i != set_integers.end())
+		cout << "Element " << *another_find_i << " found!" << endl;
+	else
+		cout << "Element 12345 not found in set!" << endl;
+
+}
+
+void example4()
+{
+	MSETINT mset_integers;
+
+	//initial mset_integers
+	mset_integers = { 43,78,78,-1,124,-1 };
+
+	cout << "multiset contains " << mset_integers.size() << " elements.";
+	cout << " There are: " << endl;
+
+	//write contents of the multiset to the screen
+	DisplayContents(mset_integers);
+
+	cout << "Please enter a number to be erased from the set" << endl;
+	int number_to_erase = 0;
+	cin >> number_to_erase;
+
+	cout << "Erasing " << mset_integers.count(number_to_erase);
+	cout << " instances of value " << number_to_erase << endl;
+
+	//try finding an element
+	mset_integers.erase(number_to_erase);
+
+	cout << "multiset contains " << mset_integers.size() << " elements.";
+	cout << " There are: " << endl;
+	DisplayContents(mset_integers);
+}
+
+ContactItem::ContactItem(const string& name, const string& number)
+{
+	contact_name_ = name;
+	phone_number_ = number;
+	display_representation_ = (contact_name_ + ": " + phone_number_);
+}
+string ContactItem::get_phone_number()const
+{
+	return this->phone_number_;
+}
+bool ContactItem::operator == (const ContactItem& item_to_compare) const
+{
+	return (item_to_compare.contact_name_ == this->contact_name_);
+}
+bool ContactItem::operator <(const ContactItem& item_to_compare)const
+{
+	return(this->contact_name_ < item_to_compare.contact_name_);
+}
+ContactItem::operator const char *()const
+{
+	return display_representation_.c_str();
+}
+void example5()
+{
+	set<ContactItem> set_contacts;
+	set_contacts.insert(ContactItem("Jack Welsch", "+1 7889 879 879"));
+	set_contacts.insert(ContactItem("Bill Gates", "+1 97 7897 8799 8"));
+	set_contacts.insert(ContactItem("Angela Merkel", "+49 23456 5466"));
+	set_contacts.insert(ContactItem("Vladimir Putin", "+7 6645 4564 797"));
+	set_contacts.insert(ContactItem("Manmohan Singh", "+91 234 4564 789"));
+	set_contacts.insert(ContactItem("Barck Obama", "+1 745 641 314"));
+	DisplayContents_n(set_contacts);
+
+	cout << "Enter a person whose number you wish to delete: ";
+	string name_input;
+	getline(cin, name_input);
+
+	auto contact_found_i = set_contacts.find(ContactItem(name_input, ""));
+	if (contact_found_i != set_contacts.end())
+	{
+		//erase the contact found in set
+		set_contacts.erase(contact_found_i);
+		cout << "Displaying contents after erasing: " << name_input << endl;
+		DisplayContents_n(set_contacts);
+	}
+	else
+		cout << "Contact not found" << endl;
+}
---
+++ chapter19/examples.h
@@ -0,0 +1,72 @@
+#ifndef EXAMPLES_H_
+#define EXAMPLES_H_
+
+#include <set>
+#include <iostream>
+#include <string>
+using namespace std;
+
+//used as a template parameter in set / mutiset instantiation
+template <typename T>
+struct SortDesending
+{
+	bool operator ()(const T& lhs, const T& rhs) const
+	{
+		return (lhs > rhs);
+	}
+};
+
+template <typename T>
+void DisplayContents(const T& input)
+{
+	for (auto element_i = input.cbegin()//auto, cbegin: c++11
+		; element_i != input.cend() //cend() is new in c++11
+		; ++element_i)
+		cout << *element_i << ' ';
+
+	cout << endl;
+}
+
+template <typename T>
+void DisplayContents_n(const T& input)
+{
+	for (auto element_i = input.cbegin()//auto, cbegin: c++11
+		; element_i != input.cend() //cend() is new in c++11
+		; ++element_i)
+		cout << *element_i << endl;
+
+	cout << endl;
+}
+
+void example1();
+void example2();
+void example3();
+
+typedef multiset <int> MSETINT;
+void example4();
+
+struct ContactItem
+{
+private:
+	string contact_name_;
+	string phone_number_;
+	string display_representation_;
+
+public:
+	//constructor and destructor
+	ContactItem(const string& name, const string& number);
+
+	string get_phone_number() const;
+
+	//used by list::remove() given contact list item
+	bool operator == (const ContactItem& item_to_compare) const;
+
+	//used by list::sort() without parameters
+	bool operator <(const ContactItem& item_to_compare)const;
+
+	//used in displaycontents via cout
+	operator const char*() const;
+};
+void example5();
+
+#endif // !EXAMPLES_H_
